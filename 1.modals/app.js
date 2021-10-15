eb427e02c001d16022031d07d66fd40b796850da
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2021-10-15 17:32:45 -0500

    first commit

---
+++ 1.modals/app.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', initializeApp);
+
+function initializeApp(){
+    const modal = new Modal(document.querySelector('.modal'));
+    modal.init();
+}
+class Modal {
+    constructor(modal){
+        // references of the differents elements on screen
+        this.modalWrapper = modal.parentElement;
+        this.openButton = modal.parentElement.previousElementSibling;
+        this.closeButton = modal.firstElementChild;
+        this.submitButton = modal.querySelector('#modal-submit');
+    }
+
+    init(){
+        this.doEventBindings();
+    }
+
+    showModal(){
+        console.log('opening the modal....');
+        this.modalWrapper.style.display = 'block';
+    }
+    closeModal(){
+        console.log('closing the modal....');
+        this.modalWrapper.style.display = 'none';
+    }
+
+    doEventBindings(){
+        this.openButton.addEventListener('click', this.showModal.bind(this));
+        this.closeButton.addEventListener('click', this.closeModal.bind(this));
+        this.submitButton.addEventListener('click', this.closeModal.bind(this));
+        window.addEventListener('click', event => 
+        {
+            if(event.target === this.modalWrapper){
+                this.closeModal();
+            }else{
+               return false; 
+            }
+        });
+        window.addEventListener('keydown', event => {
+            if (event.key === "Escape") {
+                this.closeModal();
+            }
+        });
+    }
+
+}
